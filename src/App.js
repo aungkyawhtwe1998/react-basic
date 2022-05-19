ae612dadb53104d3ee6f7096c6a8ff851eb8dcf0
aungkyawhtwe1998 <aungkyawhtwe.mdy49@gmail.com>
2022-05-19 09:21:24 +0630

    rt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
-import React, { useState, useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {login, logout} from './redux/loginReducer'
 function App() {
 
+    const loggedIn = useSelector(state => state.login);
+    const dispatch = useDispatch();
     return (
 
         <div className="container">
-
+            <h1>{loggedIn.value ? 'Member':"Guest"}</h1>
+            <button onClick={()=>dispatch(login(true))}>Login</button>
+            <button onClick={()=>dispatch(logout(false))}>Logout</button>
         </div>
     );
 }
---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import loginReducer from "./loginReducer";
+export const store = configureStore({
+    reducer:{
+        login:loginReducer
+    }
+})
